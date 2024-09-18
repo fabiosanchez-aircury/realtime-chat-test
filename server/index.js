9810e6564ce7adb8287be5697aaebd21193199fe
fabiosanchez-aircury <fabio.sanchez@aircury.com>
2024-09-18 09:44:55 +0200

    Add websocket dependency

--- server/index.js
+++ server/index.js
@@ -1,15 +1,25 @@
 import express from 'express'
 import logger from 'morgan'
 
+import { createServer } from 'node:http'
+import { Server } from 'socket.io'
+
 const port = process.env.PORT ?? 3000;
 
 const app = express();
+const server = createServer(app)
+const io = new Server(server)
+
+io.on('connection', (socket) => {
+    console.log('a user connected')
+})
+
 app.use(logger('dev'));
 
 app.get('/', (req, res) => {
-  res.send('<h1>CHat</h1>');
+  res.sendFile(process.cwd() + '/client/index.html');
 });
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Started at ${port}`);
 });
